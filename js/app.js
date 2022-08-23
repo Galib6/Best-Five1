9c90c16414fb6c94626f4adf5812fdd226964075
Galib6 <asadullahalgalib81@gmail.com>
2022-08-23 17:20:05 +0600

    Final(massive-change-in-js)

--- js/app.js
+++ js/app.js
@@ -1,96 +1,19 @@
-// Player envent handler start
 var i = 0;
-document.getElementById('messi').addEventListener('click', function () {
+function addToSelected(element) {
     if (i < 5) {
-        playerListUpdate('name-messi');
-        document.getElementById('messi').setAttribute('disabled', '');
+        const playerName = element.parentNode.parentNode.children[0].innerText;
+        //  adding to list 
+        const ul = document.getElementById("list");
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(playerName));
+        ul.appendChild(li);
+        i = i + 1;
+        document.getElementById(element.id).setAttribute('disabled', '');
     }
     else {
         alert("Only 5 player can be selected");
     }
-})
-
-document.getElementById('neymar').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-neymar');
-        document.getElementById('neymar').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
-document.getElementById('mbappe').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-mbappe');
-        document.getElementById('mbappe').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
-document.getElementById('vitor').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-vitor');
-        document.getElementById('vitor').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
-document.getElementById('ramos').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-ramos');
-        document.getElementById('ramos').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
-document.getElementById('renato').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-renato');
-        document.getElementById('renato').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
-
-document.getElementById('ronaldo').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-ronaldo');
-        document.getElementById('ronaldo').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
-document.getElementById('salah').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-salah');
-        document.getElementById('salah').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
-document.getElementById('pogba').addEventListener('click', function () {
-    if (i < 5) {
-        playerListUpdate('name-pogba');
-        document.getElementById('pogba').setAttribute('disabled', '');
-    }
-    else {
-        alert("Only 5 player can be selected");
-    }
-})
-
+}
 
 // Player event handler end
 document.getElementById('player-expence').addEventListener('click', function () {
@@ -106,6 +29,7 @@ document.getElementById('player-expence').addEventListener('click', function () 
 
     }
 })
+
 
 document.getElementById('calculate-total').addEventListener('click', function () {
     const managerFeild = document.getElementById('manager-expense');
